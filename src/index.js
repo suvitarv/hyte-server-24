41369ac33d7e7439ae29ae4d945f44d404274f14
suvitarv <suvitarv@metropolia.fi>
2024-01-23 10:13:31 +0200

    Lisätty tehtäviä

--- src/index.js
+++ src/index.js
@@ -1,21 +1,62 @@
 // index.js
-import http from 'http';
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
 const hostname = '127.0.0.1';
 const port = 3000;
 const app = express();
 
-app.get('/items', (req, res) => {
-  res.json([
-    {}
-  ])
-})
+app.use(express.static('public'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use('/sivusto', express.static(path.join(__dirname, '../public')));
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+const items = [
+  {id: 1, name: 'item1'},
+  {id: 2, name: 'item2'},
+  {id: 3, name: 'item kolme'},
+  {id: 4, name: 'item neljä'},
+];
+
+app.get('/items/', (req, res) => {
+  res.json(items);
 });
 
-server.listen(port, hostname, () => {
+app.get('/items/:id', (req, res) => {
+  console.log('request item id', req.params.id);
+  let item = items.find(item => item.id === parseInt(req.params.id));
+  if (item) {
+    res.json(item.name);
+  } else {
+    res.status(404).json({error: '404 not found'});
+  }
+});
+
+app.get('/items', (req, res) => {
+  console.log('request item id', req.params.id);
+  let item = 'tähän oikea objekti!'
+  res.json(items);
+});
+
+//itemin lisäys POST http://127.0.0.1:3000/items/
+app.post('/items', (req, res) => {
+  res.json({message: 'item created'});
+});
+
+app.get('/items', (req, res) => {
+  res.json([
+    {id: 1, name: 'item1'},
+    {id: 2, name: 'item2'},
+  ]);
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my rest API');
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
